f0786f9de69bfc061222df7a7d03c4a1bdc7437e
Swarna2503 <swarnadurga.nallam@gmail.com>
2026-03-05 05:40:37 -0600

    Update app.py

--- app.py
+++ app.py
@@ -9,7 +9,6 @@ st.title("📊 Miracle Analyst AI")
 # 2. Your Deployed Backend URL
 # We use the /run endpoint to ensure the agent actually executes its logic
 API_URL = "https://adk-default-service-name-43463140793.us-east1.run.app/apps/ecommerce_agent/users/swarna/sessions/chat_01/run"
-
 # 3. Setup Chat Memory (Session State)
 if "messages" not in st.session_state:
     st.session_state.messages = [
